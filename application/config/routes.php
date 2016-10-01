ffae73b30270cf921cc5a8131be64b09033b3434
Lei Zhang(Daniel) <danielvmaple@gmail.com>
2016-10-01 11:04:12 -0700

    fixed issue 5
    Added wildcard routing first/gimme/$1, gimme(id) function in
    controllers/First.
    fixed $route['lock/em/up'] = 'welcome/shucks' to
    $route['lock/(:any)/(:any)'] = 'welcome/shucks';;

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,5 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['sleep'] = 'first/zzz';
-$route['lock/em/up'] = 'welcome/shucks';
+$route['lock/(:any)/(:any)'] = 'welcome/shucks';
+$route['show/(:num)'] = 'first/gimme/$1';
